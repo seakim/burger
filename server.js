21a21b7318ecf7ffa8bf2317a9af518118a096b4
sk <dev.seakim@gmail.com>
2018-08-19 23:59:35 -0400

    port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3123;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
